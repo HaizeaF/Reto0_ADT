75f490cc77b3c1fddc65f537b826bbdbeeadce75
2dam <2dam@HZ375743.TartangaLH.eus>
2022-09-19 09:35:04 +0200

    Added exception manager and connection open close

---
+++ src/controller/ConnectionOpenClose.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import exception.ExceptionManager;
+
+public class ConnectionOpenClose {
+
+    protected ResourceBundle configFile;
+    protected String url;
+    protected String user;
+    protected String pass;
+    protected Connection con;
+    protected PreparedStatement stmt;
+
+    public ConnectionOpenClose() {
+        configFile = ResourceBundle.getBundle("controlador/config");
+        url = configFile.getString("URL");
+        user = configFile.getString("USER");
+        pass = configFile.getString("PASSWORD");
+    }
+
+    public Connection openConnection() throws ExceptionManager {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new ExceptionManager(e.getMessage());
+        }
+        return con;
+    }
+
+    public void closeConnection(PreparedStatement stmt, Connection con) throws SQLException {
+        if (stmt != null) {
+            stmt.close();
+        }
+
+        if (con != null) {
+            con.close();
+        }
+
+    }
+
+}
