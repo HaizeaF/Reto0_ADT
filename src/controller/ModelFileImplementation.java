98595caed24c990781576bc30ef6df91ae1a4f50
HaizeaF <haize@DESKTOP-8QPK9LI>
2022-09-20 08:17:11 +0200

    Model file inetrface implementation

--- src/controller/ModelFileImplementation.java
+++ src/controller/ModelFileImplementation.java
@@ -5,10 +5,50 @@
  */
 package controller;
 
+import java.util.HashMap;
+import model.Account;
+import model.Customer;
+import model.Movement;
+
 /**
  *
  * @author 2dam
  */
-public class ModelFileImplementation {
+public class ModelFileImplementation implements ModelInterface {
+
+    @Override
+    public void CreateCustomer(Customer c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Customer CheckCustomer(Integer idc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void CreateAccount(Account a, Integer idc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void AddClient(Integer idc, Integer ida) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account CheckAccount(Integer idc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void AccountMovement(Integer ida, Movement m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<Integer, Movement> CheckAccount_Movement(Integer ida) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
